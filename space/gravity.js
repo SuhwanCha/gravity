75fa31d4f647862c80a188c2c666797c3d5e16a1
Suhwan Cha <admin@gdb.kr>
2017-05-25 11:12:26 +0900

    Gravity complete

--- space/gravity.js
+++ space/gravity.js
@@ -8,11 +8,13 @@ var SunM = 1.989 * Math.pow(10,30);
 
 var LocateXdt= [], LocateYdt= [], VX= [], VY= [], AX= [], AY= [];
 
-LocateXdt[0] = 150000000;
+var flag = 1;
+
+LocateXdt[0] = 149600000000;
 LocateYdt[0] = 0;
 
 VX[0] = 0;
-VY[0] = 30.287;
+VY[0] = 30287;
 
 
 var distance = [], F = [];
@@ -36,28 +38,50 @@ $(document).ready(function(){
 
 // 처음 실행하는 함수들
 
+var left, top;
+left = initInfo['left']+(LocateXdt[0]/500000000);
+tops = initInfo['top']+(LocateYdt[0]/500000000);
 
 function initFunction(){
-  $("#earth").css('left', initInfo['left']+(LocateXdt[0]/300000));
-  $("#earth").css('top', initInfo['top']+(LocateYdt[0]/300000));
+  $("#earth").css('left', initInfo['left']+(LocateXdt[0]/500000000));
+  $("#earth").css('top', initInfo['top']+(LocateYdt[0]/500000000));
 }
 
 
 var Space = class {
   constructor(){
     this.i = 0;
-    this.dt = 1;
-    for(this.i = 0; this.i<= 300; this.i++){
-      this.Accel();
-      this.Velocity();
-      this.Position();
-      console.log((Math.pow(LocateXdt[this.i]) + Math.pow(LocateYdt[this.i])))
-    }
+    this.dt = 1000;
+    // for(this.i = 0; this.i<= 300; this.i++){
+    //   this.Accel();
+    //   this.Velocity();
+    //   this.Position();
+    //   console.log((Math.pow(AX[this.i],2) + Math.pow(AY[this.i],2)) / Math.pow(F[this.i],2));
+    // }
   }
 
   Accel(){
     AX[this.i] = F[this.i] * LocateXdt[this.i] / Math.sqrt(distance[this.i]) * (-1);
     AY[this.i] = F[this.i] * LocateYdt[this.i] / Math.sqrt(distance[this.i]) * (-1);
+    // switch (flag) {
+    //   case 1:
+    //     AX *= -1;
+    //     AY *= -1;
+    //     break;
+    //
+    //   case 2:
+    //     AY *= -1;
+    //     break;
+    //
+    //   case 3:
+    //     break;
+    //
+    //   case 4:
+    //     AX *= -1;
+    //
+    //   default:
+    //
+    // }
   }
 
   Velocity(){
@@ -66,12 +90,55 @@ var Space = class {
   }
 
   Position(){
+    // if(LocateXdt[this.i] - )
     LocateXdt[this.i+1] = LocateXdt[this.i] + VX[this.i]*this.dt;
     LocateYdt[this.i+1] = LocateYdt[this.i] + VY[this.i]*this.dt;
     distance[this.i+1] = Math.pow(LocateYdt[this.i+1],2) + Math.pow(LocateXdt[this.i+1],2);
     F[this.i+1] = G*SunM / distance[this.i+1];
+    // console.log((tops + LocateYdt[this.i]/500000000))
+  }
+
+  Move(){
+    $("#earth").css('left', initInfo['left'] + LocateXdt[this.i]/400000000);
+    $("#earth").css('top', initInfo['top'] - LocateYdt[this.i]/400000000);
+  }
+
+  Text(){
+    $(".ax").html(AX[this.i]);
+    $(".ay").html(AY[this.i]);
+    $(".vx").html(VX[this.i]);
+    $(".vy").html(VY[this.i]);
+    $(".sx").html(LocateXdt[this.i]);
+    $(".sy").html(LocateYdt[this.i]);
   }
 
 }
 
+$('.info').tooltip({html: true})
 var Space = new Space();
+function start(){
+  SunM = parseInt($("#m1").val());
+  EarthM = parseInt($("#m2").val());
+  LocateXdt[0] = parseInt($("#sx").val());
+  LocateYdt[0] = parseInt($("#sy").val());
+  VX[0] = parseInt($("#vx").val());
+  VY[0] = parseInt($("#vy").val());
+
+  distance[0] = Math.pow(LocateYdt[0],2) + Math.pow(LocateXdt[0],2);
+  F[0] = G*SunM / distance[0];
+  AY[0] = F[0] * LocateYdt[0] * -1;
+  AX[0] = F[0] * LocateXdt[0] * -1;
+
+  $("#set").hide();
+
+  setInterval(function(){
+    Space.Accel();
+    Space.Velocity();
+    Space.Position();
+    Space.Move();
+    Space.Text();
+    Space.i++;
+    // console.log(Space.i);
+  },1)
+
+}
